01897643a0d7786762e9065a18d639739dcc787a
Jay Savaliya <jrsavaliya04@gmail.com>
2023-04-23 17:36:20 +0530

    loder file done

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,9 +1,16 @@
+// import PropTypes from 'prop-types'
 import React from 'react'
+import loading from './Fading wheel.gif'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <>
+     <div className="text-center">
+                <img src={loading} alt="loading" srcSet="" />
+            </div>
+    </>
   )
 }
 
 export default Loader
+
