a14cc5faac8bccb831ec5556c7f56967b07dfcb4
Jay Savaliya <jrsavaliya04@gmail.com>
2023-04-27 06:55:23 +0530

    news item updated

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const NewsItem = () => {
-  return (
-    <div>NewsItem</div>
-  )
+const NewsItem = (props) => {
+
+    let { title, description, imgUrl, newsUrl } = props;
+    return (
+        <>
+            <div className="card mt-3 my-3">
+                <img src={!imgUrl ? "https://wallpapers.com/images/hd/simple-city-outline-0160co3ohzvkh0jj.jpg" : imgUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+                </div>
+            </div>
+        </>
+    )
+
 }
 
 export default NewsItem
