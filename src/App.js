be07131140fc00144843a3ef020945f7f2f98aff
Jay Savaliya <jrsavaliya04@gmail.com>
2023-04-28 09:03:27 +0530

    app js update 2 add route move one page to page

--- src/App.js
+++ src/App.js
@@ -30,3 +30,42 @@ const App = () => {
                 <News setProgress={setProgress} key="general" pageSize={pageSize} category="general" />
               }
               />
+              <Route exact path="/business" element={
+                <News setProgress={setProgress} key="business" pageSize={pageSize} category="business" />
+              }
+              />
+              <Route exact path="/entertainment" element={
+                <News setProgress={setProgress} key="entertainment" pageSize={pageSize} category="entertainment" />
+              }
+              />
+              <Route exact path="/general" element={
+                <News setProgress={setProgress} key="general" pageSize={pageSize} category="general" />
+              }
+              />
+              <Route exact path="/health" element={
+                <News setProgress={setProgress} key="health" pageSize={pageSize} category="health" />
+              }
+              />
+              <Route exact path="/science" element={
+                <News setProgress={setProgress} key="science" pageSize={pageSize} category="science" />
+              }
+              />
+              <Route exact path="/sports" element={
+                <News setProgress={setProgress} key="sports" pageSize={pageSize} category="sports" />
+              }
+              />
+              <Route exact path="/technology" element={
+                <News setProgress={setProgress} key="technology" pageSize={pageSize} category="technology" />
+              }
+              />
+            </Routes>
+          </Router>
+
+        </div>
+      </>
+    )
+  }
+
+
+
+export default App
