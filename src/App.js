acfad4fe191bc4c686287b252734a2bee85ff6a6
Jay Savaliya <jrsavaliya04@gmail.com>
2023-04-28 09:02:07 +0530

    app js updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <>
-    <h1>jay savaliya</h1>
-    </>
-    
-  );
-}
+import React ,{useState} from 'react'
+import Navbar from './Components/Navbar';
+import News  from './Components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
-export default App;
+const App = () => {
+  // const c = 'jay';
+  const pageSize = 5;
+  const [progress, setProgress] = useState(0)
+  
+    return (
+      <>
+        <div>
+          <Router>
+            <Navbar />
+            <LoadingBar
+              color='#f11946'
+              progress={progress}
+              height={3}
+            />
+            <Routes>
+              <Route exact path="/" element={
+                <News setProgress={setProgress} key="general" pageSize={pageSize} category="general" />
+              }
+              />
